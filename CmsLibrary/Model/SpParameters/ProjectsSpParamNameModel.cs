1c1677a243b91b333d229a77e2e67044da0e3735
peter john r. hugo <johnayossulit@gmail.com>
2019-10-10 16:01:54 +0800

    added and refactored

---
+++ CmsLibrary/Model/SpParameters/ProjectsSpParamNameModel.cs
@@ -0,0 +1,54 @@
+﻿using CmsLibrary.Interface.SpParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsLibrary.Model.SpParameters {
+    public class ProjectsSpParamNameModel : IProjectsSpParamName {
+        public string DateStarted {
+            get {
+                return "@DateStarted";
+            }
+        }
+
+        public string events {
+            get {
+                return "@event";
+            }
+        }
+
+        public string Id {
+            get {
+                return "@Id";
+            }
+        }
+
+        public string MainProjectId {
+            get {
+                return "@MainProjectId";
+            }
+        }
+
+        public string ProjectName {
+            get {
+                return "@ProjectName";
+            }
+        }
+
+        public string SubProjectName {
+            get {
+                return "@SubProjectName";
+            }
+        }
+
+        public string TableName {
+            get {
+                return "@TableName";
+            }
+        }
+
+
+    }
+}
